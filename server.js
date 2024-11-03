752eb8f6f56e22a225a192f57c720a4c91369dd5
vishnugnair <119111934+vishnugnair@users.noreply.github.com>
2024-11-04 00:54:54 +0530

    serverjs updated

--- server.js
+++ server.js
@@ -1,43 +1,36 @@
 import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
+import path from "path";
 import authRoutes from "./routes/authRoutes.js";
 import eventRoutes from "./routes/eventRoutes.js";
 import dotenv from "dotenv";
-import path from "path";
 
-dotenv.config();
 const app = express();
+
 app.use(cors());
 app.use(express.json());
+dotenv.config();
 
 const mongoURI = process.env.MONGOURI;
 mongoose
-  .connect(mongoURI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(mongoURI)
   .then(() => console.log("MongoDB connected"))
   .catch((error) => console.error("MongoDB connection error:", error));
 
 app.use("/api/auth", authRoutes);
 app.use("/api/events", eventRoutes);
 
-// Serve static files from the React app's build folder in production
-if (process.env.NODE_ENV === "production") {
-  const __dirname = path.resolve();
-  app.use(express.static(path.join(__dirname, "/client/build")));
+// Serve static files from the React app
+const __dirname = path.resolve();
+app.use(express.static(path.join(__dirname, "client/build")));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
-  });
-} else {
-  app.get("/", (req, res) => {
-    res.send("Server is running");
-  });
-}
+// Catch-all handler to serve the React app for any unknown routes
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client/build", "index.html"));
+});
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
